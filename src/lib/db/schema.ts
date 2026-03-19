4502bc200a0b2cb5cac4f9aa862d6b77972d945c
Alexander Kern <alexander.t.kern@gmail.com>
2026-03-19 10:28:46 -0500

    feed_follows table schema and migration

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,6 @@
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import { foreignKey } from "drizzle-orm/gel-core";
+import { pgTable, timestamp, uuid, text, unique} from "drizzle-orm/pg-core";
+import { table } from "node:console";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom().notNull(),
@@ -21,3 +23,17 @@ export const feeds = pgTable("feeds", {
     url: text("url").notNull().unique(), 
     userId: uuid("user_id").notNull().references(() => users.id, {onDelete: "cascade"})
 });
+
+export const feed_follows = pgTable("feed_follows", {
+    id: uuid("id").primaryKey().defaultRandom().notNull(), 
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+        .notNull()
+        .defaultNow()
+        .$onUpdate(() => new Date()),
+    userId: uuid("user_id").notNull().references(() => users.id, {onDelete: "cascade"}), 
+    feedId: uuid("feed_id").notNull().references(() => feeds.id, {onDelete: "cascade"}),
+}, (table) => ({
+        unq: unique().on(table.userId, table.feedId)
+    })
+)
